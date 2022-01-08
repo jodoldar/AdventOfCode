1f0b3c61e9e4fad6c926e89a0ae6b8303c469487
Josep Dols <jodoldar@users.noreply.github.com>
2022-01-08 13:38:09 +0100

    Added first try of Day 16

--- Common/utils.hpp
+++ Common/utils.hpp
@@ -39,3 +39,44 @@ static inline std::string trim_copy(std::string s) {
     trim(s);
     return s;
 }
+
+int bin2dec(std::string number)
+{
+    int ret_value = 0;
+    int base = 1;
+
+    for (int i = number.length() - 1; i >= 0; i--)
+    {
+        if (number[i] == '1')
+        {
+            ret_value += base;
+        }
+        base *= 2;
+    }
+
+    return ret_value;
+}
+
+static inline std::string char_to_bin(char s) {
+    switch (s)
+    {
+        case '0': return "0000";
+        case '1': return "0001";
+        case '2': return "0010";
+        case '3': return "0011";
+        case '4': return "0100";
+        case '5': return "0101";
+        case '6': return "0110";
+        case '7': return "0111";
+        case '8': return "1000";
+        case '9': return "1001";
+        case 'A': return "1010";
+        case 'B': return "1011";
+        case 'C': return "1100";
+        case 'D': return "1101";
+        case 'E': return "1110";
+        case 'F': return "1111";
+    }
+
+    return "";
+}
